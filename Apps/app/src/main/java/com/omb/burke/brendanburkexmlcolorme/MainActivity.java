9f1a77a075df6c99fdbe97dcbd651f36dffaa492
burke212 <burke212@live.com>
2018-03-21 15:09:49 -0400

    Touch ups

--- Apps/app/src/main/java/com/omb/burke/brendanburkexmlcolorme/MainActivity.java
+++ Apps/app/src/main/java/com/omb/burke/brendanburkexmlcolorme/MainActivity.java
@@ -10,12 +10,14 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button redButton;
     private Button blueButton;
     private Button greenButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,12 +54,18 @@ public class MainActivity extends AppCompatActivity {
         switch (view.getId()){
             case R.id.red_btn:
                 redButton.setBackgroundColor(Color.RED);
+                Toast.makeText(MainActivity.this, "RED",
+                        Toast.LENGTH_SHORT).show();
                 break;
             case R.id.green_btn:
                 greenButton.setBackgroundColor(Color.GREEN);
+                Toast.makeText(MainActivity.this, "GREEN",
+                        Toast.LENGTH_SHORT).show();
                 break;
             case R.id.blue_btn:
                 blueButton.setBackgroundColor(Color.BLUE);
+                Toast.makeText(MainActivity.this, "BLUE",
+                        Toast.LENGTH_SHORT).show();
                 break;
             default:
                 break;
